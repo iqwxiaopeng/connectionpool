76d389d165b8bc91e8778b938481e47c85c391c7
TraceBundy <yangzhou221@gmail.com>
2015-04-13 18:36:26 +0800

    func

--- connection_pool.h
+++ connection_pool.h
@@ -6,20 +6,27 @@
 #include <mysql_connection.h>
 #include <nocopyable.h>
 #include <boost/shared_ptr.hpp>
+#include <boost/function.hpp>
+
 typedef boost::shared_ptr<MySQLConn> Connection;
 typedef std::list<Connection> ConnectionList;
+typedef boost::function<void()> HighWaterMarkCallback;
+
 class ConnectionPool:NonCopyable
 {
-public:
-
 public:
 	ConnectionPool() {};
 	~ConnectionPool() {};
 	Connection GetConnection();
+	void SetHighWaterMarkCallback(const HighWaterMarkCallback &cb){
+		highWaterMarkCallback_ = cb; cb();
+	}
+	int ReleaseConnection(const Connection &conn){};
 private:
 	const static int HighWaterMark = 1024;
 private:
 	ConnectionList list_;
 	MutexLock lock_;
+	HighWaterMarkCallback highWaterMarkCallback_;
 };
 #endif
--- main.cc
+++ main.cc
@@ -1,12 +1,16 @@
 #include <connection_pool.h>
-
+#include <boost/bind.hpp>
 #include <iostream>
 using namespace std;
-
-
+using namespace boost;
+void echo()
+{
+	cout << "hello world" << endl;
+}
 int main()
 {
     ConnectionPool pool;
     pool.GetConnection();
+	pool.SetHighWaterMarkCallback(bind(&echo));
     return 0;
 }
